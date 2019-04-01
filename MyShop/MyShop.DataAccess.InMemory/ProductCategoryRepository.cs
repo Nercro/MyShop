85ff7e59765951f8d97094fbebd1346e202a2042
Nercro <37053252+Nercro@users.noreply.github.com>
2019-04-01 13:40:57 +0200

    Product grouping added

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,89 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories;
+
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        //stores data in cache
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+
+        //inserts new product in list
+        public void Insert(ProductCategory newProductCategory)
+        {
+            productCategories.Add(newProductCategory);
+        }
+
+        //updates existitng product from list
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productCategoryToUpdate = productCategories.Find(p => p.ID == productCategory.ID);
+
+            if (productCategoryToUpdate != null)
+            {
+                productCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+
+        //finds and returns project by ID
+        public ProductCategory Find(string id)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.ID == id);
+
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+
+        //returns products list as queryable
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+
+        //deletes product from list
+        public void Delete(string id)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.ID == id);
+
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -12,10 +13,12 @@ namespace MyShop.WebUI.Controllers
     {
 
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -26,9 +29,12 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
 
-            return View(product);
+            viewModel.product = new Product();
+            viewModel.productCategories = productCategories.Collection();
+
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -57,7 +63,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+                viewModel.product = product;
+                viewModel.productCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
 
