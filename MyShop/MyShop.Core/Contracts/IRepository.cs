b430e31527fcaaf62064a8db3267a11cd7a6eb6e
Nercro <37053252+Nercro@users.noreply.github.com>
2019-04-01 15:24:32 +0200

    Implemented Microsoft Unity

---
+++ MyShop/MyShop.Core/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using MyShop.Core.Models;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IRepository<T1> where T1 : BaseEntity
+    {
+        IQueryable<T1> Collection();
+        void Commit();
+        void Delete(string id);
+        T1 Find(string id);
+        void Insert(T1 t);
+        void Update(T1 t1);
+    }
+}
