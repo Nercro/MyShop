0e51057470a695eb250395c5fc7172bf2abdab87
Nercro <37053252+Nercro@users.noreply.github.com>
2019-04-02 14:57:26 +0200

    Created product filter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +21,26 @@ namespace MyShop.WebUI.Controllers
 
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.products = products;
+            model.productCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string id)
